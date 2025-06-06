5f6995c5ed8ea2d3a3168729096c67b91ac856bf
YuSuzu1107 <yu20021107@gmail.com>
2025-06-06 11:20:11 +0900

    Update make_music_input.sh

--- scripts/make_music_input.sh
+++ scripts/make_music_input.sh
@@ -1,17 +1,17 @@
-# python3 scripts/my_utils/beat.py \
-# # ここに入力してください
-# --page  "https://songle.jp/songs/songle.jp%2Fuploads%2Fct8m4peiqnulo43nehig.mp3" \
-# --output_dir input/music
+python3 scripts/my_utils/beat.py \
+# ここに入力してください
+--page  "https://songle.jp/songs/songle.jp%2Fuploads%2Fct8m4peiqnulo43nehig.mp3" \
+--output_dir input/music
 
-# python3 scripts/my_utils/sabi.py \
-# # ここに入力してください
-# --page  "https://songle.jp/songs/songle.jp%2Fuploads%2Fct8m4peiqnulo43nehig.mp3" \
-# --output_dir input
+python3 scripts/my_utils/sabi.py \
+# ここに入力してください
+--page  "https://songle.jp/songs/songle.jp%2Fuploads%2Fct8m4peiqnulo43nehig.mp3" \
+--output_dir input
 
-# python3 scripts/my_utils/music.py \
-# --audio input/music.wav \
-# --motion input/raw.json \
-# --output input/music/music.json
+python3 scripts/my_utils/music.py \
+--audio input/music.wav \
+--motion input/raw.json \
+--output input/music/music.json
 
 python3 scripts/my_utils/frameinterval.py \
 --beat input/music/beat.json \
